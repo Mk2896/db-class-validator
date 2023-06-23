fb0c531bc80d693b6926f223e0b3146e9784f07a
maaz khan <maazkhan580@gmail.com>
2023-06-23 21:01:00 +0500

    Set index.ts and created npmignore file

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { Exists } from './decorators/exists.decorator';
+import { ExistsValidation } from './validators/exists.validator';
+
+
+export { Exists } from './decorators/exists.decorator';
+export { ExistsValidation } from './validators/exists.validator';
