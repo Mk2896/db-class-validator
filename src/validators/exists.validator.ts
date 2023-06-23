0a9cc191eb37e304c8e009c83c1470571580111c
maaz khan <maazkhan580@gmail.com>
2023-06-23 20:36:49 +0500

    Setup tsconfig

--- src/validators/exists.validator.ts
+++ src/validators/exists.validator.ts
@@ -2,24 +2,24 @@ import { Injectable } from "@nestjs/common";
 import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
 import { DataSource } from "typeorm";
 
-// @ValidatorConstraint({ name: 'Exists', async: true })
-// @Injectable()
+@ValidatorConstraint({ name: 'Exists', async: true })
+@Injectable()
 export class ExistsValidation implements ValidatorConstraintInterface {
     constructor(
         private readonly connection: DataSource
     ) { }
 
     async validate(value: any, args: ValidationArguments): Promise<boolean> {
-        const [tableName , columnName] = args.constraints;
-        if(value === undefined) {
+        const [tableName, columnName] = args.constraints;
+        if (value === undefined) {
             return false;
         }
 
         const query: string = `Select COUNT(${columnName}) as total from ${tableName} WHERE ${columnName} = "${value}"`;
-        
+
         const queryRunner = this.connection.createQueryRunner();
         const result = await queryRunner.query(query);
-            
+
         queryRunner.release();
 
         return result[0]['total'] > 0 ? true : false;
